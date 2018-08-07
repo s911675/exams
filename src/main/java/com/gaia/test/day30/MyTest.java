0d99f977d7612914f1d078bad0e6ecea13b4f8b9
BONGGYUNNOH\s911675 s911675@gmail.com <BONGGYUNNOH\s911675 s911675@gmail.com>
2018-08-07 18:02:15 +0900

    .

--- src/main/java/com/gaia/test/day30/MyTest.java
+++ src/main/java/com/gaia/test/day30/MyTest.java
@@ -1,5 +1,6 @@
 package com.gaia.test.day30;
 
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,14 @@ public class MyTest {
 	public static void main(String[] args) {
 		MyTest test = new MyTest ();
 		
-		String value = "as df af";
+		String value1 = "as df af";
+		String value2 = "as df afx";
 		
-		test.printDuplicateChars(value);;
+		test.printDuplicateChars(value1);
+		System.out.println(test.isPalindrome(value1));
+		System.out.println(test.removeDuplicated(value1));
+		System.out.println(test.isIdenticalElements(value1, value1));
+		System.out.println(test.isIdenticalElements(value1, value2));
 	}
 	
 	public void printDuplicateChars(String value) {
@@ -23,4 +29,42 @@ public class MyTest {
 		.entrySet().stream().filter(x -> x.getValue() > 1)
 		.forEach(x -> System.out.println(String.format("%c:%d", x.getKey(), x.getValue())));
 	}
+	
+	public boolean isPalindrome(String value) {
+		if(value == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		String reverseValue = new StringBuffer(value).reverse().toString();
+		
+		return value.equals(reverseValue) ? true : false;
+	}
+	
+	public String removeDuplicated(String value) {
+		if(value == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		return value.chars().mapToObj(c -> String.valueOf((char)c)).distinct().collect(Collectors.joining());
+	}
+	
+	public boolean isIdenticalElements(String a, String b) {
+		if(a == null || b == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		Set<Character> aSet = a.chars().mapToObj(c -> (char)c).collect(Collectors.toSet());
+		Set<Character> bSet = b.chars().mapToObj(c -> (char)c).collect(Collectors.toSet());
+		
+		return aSet.containsAll(bSet) && bSet.containsAll(aSet) ? true : false;
+	}
+	
+	/*
+	SELECT B.uid, B.title, B.contents, BU.name, B.created)date
+		C.uid AS do_uid, C.comment, CU.name, C.created_date
+	FROM board B, comment C, customer BU, customer CU
+	WHER B.uid = C.board_uid
+		AND B.created_by = BU.login_id
+		AND C.created_by = CU.login_id
+	 */
 }
